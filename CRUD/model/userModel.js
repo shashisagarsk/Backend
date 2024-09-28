216e488e22a931b7ac569dd652f1eaf41edb6a03
SAGAR KUMAR <sagarazm999@gmail.com>
2024-09-28 17:48:26 +0530

    backend using node js and mongos and postman api and express js

---
+++ CRUD/model/userModel.js
@@ -0,0 +1,32 @@
+// Import necessary modules
+import express from "express"; // Import Express framework
+import mongoose from "mongoose"; // Import Mongoose for MongoDB interactions
+import bodyParser from "body-parser"; // Import Body-parser for parsing request bodies
+import dotenv from "dotenv"; // Import dotenv for loading environment variables
+
+// Initialize Express app
+const app = express();
+
+// Middleware for parsing JSON request bodies
+app.use(bodyParser.json());
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Define port for the server to listen on
+const PORT = process.env.PORT || 8000;
+
+// Define MongoDB connection URL from environment variables
+const mongodb = process.env.MONGO_URL;
+
+// Connect to MongoDB database
+mongoose
+  .connect(mongodb)
+  .then(() => {
+    console.log("Database connected successfully."); // Log successful database connection
+    // Start server on specified port
+    app.listen(PORT, () => {
+      console.log(`Server is running on port : ${PORT}`); // Log server running message
+    });
+  })
+  .catch((error) => console.log(error)); // Log error if database connection fails
